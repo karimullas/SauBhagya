3e121c3f88c37605fec3bf5c37cf5336b9866e91
karimullas <karim.mca27@gmail.com>
2018-03-17 18:33:52 +0530

    New Code

---
+++ Infy/src/main/java/Saubhagya/Infy/App.java
@@ -0,0 +1,13 @@
+package Saubhagya.Infy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
